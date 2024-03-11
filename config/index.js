3621a22221e137f7289bc95dd11ed61f994d2080
unknown <dev.surajyadav@gmail.com>
2024-03-11 23:27:58 +0530

    Fix Code

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ import developmentConfig from './development.json';
 import stagingConfig from './staging.json';
 
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
-// const NODE_ENV = process.env.NODE_ENV || 'staging';
+// const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || 'staging';
 
 
 let config;
